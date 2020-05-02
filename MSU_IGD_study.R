5f711ae31bfe80b51b22bc0fa36af61f60f2b50d
Kevin Rosenfield <kar61@psu.edu>
2020-05-01 23:34:10 -0400

    second commit

---
+++ MSU_IGD_study.R
@@ -0,0 +1,179 @@
+library(dplyr); library(lmerTest); library(psych)
+
+# read in data
+
+df <- as.data.frame(read.csv("df.csv"))
+df <- df[which(df$group !="REL" | is.na(df$group)),]
+df$group<-droplevels(df$group)
+
+# change variable types
+
+headArray <- function(first,last, df = df) match(first, names(df)):match(last, names(df))
+
+df[headArray("ID", "ihh_race", df)] <- data.frame(lapply(df[headArray("ID", "ihh_race", df)], function(x) as.factor(x)))
+df[headArray("age", "cgnq24b_4_score", df)] <- data.frame(lapply(df[headArray("age", "cgnq24b_4_score", df)], function(x) as.numeric(as.character(x))))
+
+# scale scores for each question (seperately by sex)
+
+df.men <- subset(df, sex == "m")
+df.women <- subset(df, sex == "f")
+df.men[(ncol(df) + 1):(ncol(df) + 31) ] <- scale(df.men[headArray("cgnq1_score", "cgnq24b_4_score", df.men)])
+df.women[(ncol(df) + 1):(ncol(df) + 31)] <- scale(df.women[headArray("cgnq1_score", "cgnq24b_4_score", df.men)])
+df <- rbind(df.men,df.women)
+
+rm(df.men, df.women)
+
+# remove raw responses, unscaled scores; calculate  composites
+
+df <- df[-c(headArray("cgnq1_raw", "cgnq24b_4_score", df))]
+
+names(df) <- c(names(df[1:(ncol(df) - 31)]), paste("cgnq", 1:23, "_scaled", sep = ""),
+               paste("cgnq24a", 1:4, "_scaled", sep = ""), paste("cgnq24b", 1:4, "_scaled", sep = ""))
+
+df$ksoq5recode <- 7 - df$ksoq5
+df <- df[-c(match("cgnq24a2_scaled", names(df)), match("cgnq24b2_scaled", names(df)))]
+df$cgn_comp <- rowMeans(df[headArray("cgnq1_scaled", "cgnq24b4_scaled", df)], na.rm = TRUE)
+df$so <- rowMeans(df[c("Kinsey_Attr", "Kinsey_Fantasy", "ksoq5recode")], na.rm =TRUE)
+df$mean_T <- rowMeans(df[c("AM_T","PM_T")], na.rm = TRUE)
+df$all_T <- rowMeans(df[c("mean_T","ihh_T")], na.rm = TRUE)
+
+# this probs isn'tnecessary for you but I need it; turns NaNs in NAs
+
+is.nan.data.frame <- function(x) do.call(cbind, lapply(x, is.nan))
+df[is.nan(df)] <- NA
+
+# describe
+
+describe(df$cgn_comp[df$study=="ihh"])
+describe(df$cgn_comp[df$study=="msu"])
+
+# average cgn for each sex
+
+mean(df$cgn_comp[df$sex=="m"], na.rm = TRUE)
+mean(df$cgn_comp[df$sex=="f"], na.rm = TRUE)
+mean(df$all_T[df$sex=="m"], na.rm = TRUE)
+mean(df$all_T[df$sex=="f"], na.rm = TRUE)
+
+
+cgnT_ihh <- lm(cgn_comp ~ log(all_T), data = subset(df, study == "ihh" & group != "REL" & sex == "m")); summary(cgnT_ihh); hist(residuals(cgnT_ihh))
+cgnT_msu <- lmer(cgn_comp ~ log(all_T) + (1|sibID), data = subset(df, study == "msu" & sex == "m")); summary(cgnT_msu); hist(residuals(cgnT_msu))
+
+
+soT_ihh <- lm(ksoq9 ~ log(all_T), data = subset(df, study == "ihh" & group == "CON" & sex == "f")); summary(soT_ihh); hist(residuals(soT_ihh))
+soT_msu <- lmer(cgn_comp ~ all_T + (1|sibID), data = subset(df, study == "msu" & sex == "f")); summary(soT_msu); hist(residuals(soT_msu))
+
+soCGN_ihh <- lm(ksoq9 ~ cgn_comp, data = subset(df, study == "ihh" & group == "CON" & sex == "f")); summary(soCGN_ihh); hist(residuals(soCGN_ihh))
+
+soCGN_msu <- lm(so ~ cgn_comp, data = subset(df, study == "msu" & sex == "m")); summary(soCGN_msu); hist(residuals(soCGN_msu))
+
+amt_so <- lm(formula = scale(log(am_t)) ~ so); summary(amt_so)
+pmt_cgn <- lm(formula = scale(log(pm_t)) ~ cgn); summary(pmt_cgn)
+pmt_so <- lm(formula = scale(log(pm_t)) ~ so); summary(pmt_so)
+cgn_so <- lm(formula = cgn ~ so); summary(cgn_so)
+
+scatter.smooth(scale(log(am_t)),cgn)
+
+hist(MSUMEN_AMT, breaks = 20, freq = FALSE)
+cor.test(MSUMEN_AMT,MSUMEN_CGNQ, method="spearman")
+shapiro.test(MSUMEN_AMT)
+qqnorm(MSUMEN_AMT)
+hist(MSUMEN_AMT, breaks = 20, freq = TRUE)
+kurtosis(MSUMEN_AMT_RAW)
+cor.test(MSUMEN_AMT_RAW,MSUMEN_CGNQ, method="spearman")
+shapiro.test(MSUMEN_AMT_RAW)
+qqnorm(MSUMEN_AMT_RAW)
+hist(MSUMEN_AMT_RAW, breaks = 20, freq = TRUE)
+scatter.smooth(MSUMEN_AMT_RAW,MSUMEN_PMT_RAW)
+scatter(MSUMEN_AMT,MSUMEN_PMT)
+hist(MSUMEN_CGNQ, breaks = 20, freq = TRUE)
+mean(MSUMEN_CGNQ)
+mean(MSUDATA$average_24_cgnq)
+cor.test(MSUMEN_AMT, MSUMEN_CGNQ)
+rma(-0.1579)
+plot(MSUMEN_AMT,MSUMEN_CGNQ, cex=1.3, pch=19, col="brown2", main = "Testosterone plotted against CGN", xlab="Testosterone (logged and standardized)", ylab="CGN composite score", bty="n", xlim=c(-3,3), ylim=c(-1.5,1))
+lineMSU <- lm(MSUMEN_AMT ~ MSUMEN_CGNQ)
+abline(lineMSU, col="brown2", lwd=3)
+par(new=T)
+#
+#
+#
+#
+MSUDATA<- as.data.frame(read.csv("~/Documents/Penn State/CHH/MSU/MSUCSV.csv"))
+MSUMEN<- MSUDATA[(1:191),]
+# MSUMEN<- MSUMEN[-c(81, 108, 125, 126, 127),]
+#127 has CGNQ 5 SD lower than mean; 108, 125, 126 have AM way higher than PMtest; 81 possible blood contamination
+MSUMEN_CGNQ <- as.numeric(as.character((MSUMEN[,9])))
+MSUMEN_AMT_RAW <- as.numeric(as.character((MSUMEN[,10])))
+MSUMEN_PMT_RAW <- as.numeric(as.character((MSUMEN[,11])))
+MSUMEN_AMT <- as.numeric(as.character((MSUMEN[,14])))
+MSUMEN_PMT <- as.numeric(as.character((MSUMEN[,15])))
+MSUMEN_ORIENT <- as.numeric(as.character((MSUMEN[,16])))
+MSUMEN_VAR <- data.frame(MSUMEN_CGNQ, MSUMEN_AMT, MSUMEN_AMT_RAW, MSUMEN_PMT, MSUMEN_PMT_RAW, MSUMEN_ORIENT)
+colnames(MSUMEN_VAR) <- c("CGNQ", "AMTEST", "AMTESTRAW", "PMTEST", "PMTESTRAW", "ORIENT")
+MSUMEN_VAR
+MSUMEN_AMTxCGNQ <- lm(formula = AMTEST ~ CGNQ, data = MSUMEN_VAR)
+MSUMEN_AMTxORIENT <- lm(formula = AMTEST ~ ORIENT, data = MSUMEN_VAR)
+MSUMEN_PMTxCGNQ <- lm(formula = PMTEST ~ CGNQ, data = MSUMEN_VAR)
+MSUMEN_PMTxORIENT <- lm(formula = PMTEST ~ ORIENT, data = MSUMEN_VAR)
+MSUMEN_AMTxCGNQxORIENT <- lm(CGNQ ~ MSUMEN_AMT + ORIENT, data = MSUMEN_VAR)
+summary(MSUMEN_AMTxCGNQ)
+summary(MSUMEN_AMTxORIENT)
+summary(MSUMEN_CGNQxORIENT)
+summary(MSUMEN_PMTxCGNQ)
+summary(MSUMEN_PMTxORIENT)
+summary(MSUMEN_AMTxCGNQxORIENT)
+scatter.smooth(MSUMEN_AMT,MSUMEN_CGNQ)
+hist(MSUMEN_AMT, breaks = 20, freq = FALSE)
+cor.test(MSUMEN_AMT,MSUMEN_CGNQ, method="spearman")
+shapiro.test(MSUMEN_AMT)
+qqnorm(MSUMEN_AMT)
+hist(MSUMEN_AMT, breaks = 20, freq = TRUE)
+kurtosis(MSUMEN_AMT_RAW)
+cor.test(MSUMEN_AMT_RAW,MSUMEN_CGNQ, method="spearman")
+shapiro.test(MSUMEN_AMT_RAW)
+qqnorm(MSUMEN_AMT_RAW)
+hist(MSUMEN_AMT_RAW, breaks = 20, freq = TRUE)
+scatter.smooth(MSUMEN_AMT_RAW,MSUMEN_PMT_RAW)
+scatter(MSUMEN_AMT,MSUMEN_PMT)
+hist(MSUMEN_CGNQ, breaks = 20, freq = TRUE)
+mean(MSUMEN_CGNQ)
+mean(MSUDATA$average_24_cgnq)
+cor.test(MSUMEN_AMT, MSUMEN_CGNQ)
+rma(-0.1579)
+plot(MSUMEN_AMT,MSUMEN_CGNQ, cex=1.3, pch=19, col="brown2", main = "Testosterone plotted against gender conformity", xlab="Testosterone Z-scores", cex.lab=1.5, ylab="Gender conformity", bty="n", xlim=c(-3,2.6), ylim=c(-1,1), mgp = c(2.3, 1, 0))
+lineMSU <- lm(MSUMEN_AMT ~ MSUMEN_CGNQ)
+abline(lineMSU, col="brown2", lwd=3)
+par(new=T)
+
+ggplot(MSUMEN, aes(x = MSUMEN_AMT, y = MSUMEN_CGNQ)) + 
+  geom_point(color="red", size=3) +
+  stat_smooth(method = "lm", color = "red") + 
+  xlim(-3, 3.2) +
+  ylim(-1, 1) +
+  ggtitle("Testosterone plotted against gender conformity", subtitle = NULL) +
+  theme(plot.title = element_text(face="bold", size=18, hjust=.4)) +
+  xlab("Testosterone Z-scores") +
+  ylab("Gender conformity") +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14,face="bold", vjust=.1)) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+par(new=T)
+
+ggplot() + points(MSUMEN_VAR, aes(x = AMTEST, y = CGNQ))
+length(MSUMEN_VAR$CGNQ)
+length(MSUMEN_VAR$AMTEST)
+CHH_FRAME <- data.frame(c(ALL_VAR$CGNQ,0,0), c(ALL_VAR$TEST,0,0))
+ggplot(MSUMEN, aes(x = MSUMEN_AMT, y = MSUMEN_CGNQ)) +
+         geom_point() +
+         geom_point(data = CHH_FRAME, color = "red")
+comp7xT_MSU <- lm(MSUMEN_COMP7 ~ MSUMEN_AMT)
+summary(comp7xT_MSU)
+cor.test(MSUMEN_COMP7, MSUMEN_AMT)
+mean(MSUMEN_CGNQ)
+sd(MSUMEN_CGNQ)
+mean(MSUMEN_AMT)
+sd(MSUMEN_AMT)
