7801bcfd2d1d05908ba1aec1aa87a3af0a7026d9
Kevin Rosenfield <kar61@psu.edu>
2020-09-15 11:57:57 -0400

    combined function scripts

---
+++ eclipse-workspace/VersionControlTest/src/VersionControlTestClass.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class VersionControlTestClass {
+	
+	public static void main(String[] args) {
+		
+		Scanner countup = new Scanner(System.in);
+		
+		int countint = countup.nextInt() + 1;
+		
+		System.out.println(countint);
+		
+	}
+
+}
